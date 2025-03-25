6c2b086869f277b2710649e4e23d8b45b0b43a2e
indra <indra.works2020@gmail.com>
2025-03-25 14:54:20 +0700

    loading opactiy improvement

--- src/components/common/LoadingDots.js
+++ src/components/common/LoadingDots.js
@@ -14,65 +14,96 @@ adi sprti kedap kedip pindah dari opacity 0 ke 1
 
 const LoadingDots = () => {
   //nilai value yg dipakai dari reference useREf adalah nilai current dari itu sellau
+
   const dot1Opacity = useRef(new Animated.Value(0)).current;
   const dot2Opacity = useRef(new Animated.Value(0)).current;
   const dot3Opacity = useRef(new Animated.Value(0)).current;
+  //kita ganti diatas jadi btuk array jadi
+  //kita mau buat array 3 di fill isi langusng masuk ke variable cojtoh  let a = array(3).fill(1)
+  //berartu buat sbuah array index sbnyak 3,dan di isi masing element dgn angka 1
+  //yg imi kita buat Array element 3,diisi dgn angka 0 ( opacirty =0)
+  //nah lantas kita masukan ke useRef agar tak usah lagi render component jika update angka2 ini /variable ini!
+
+  const dotOpacities = useRef(
+    Array(3)
+      .fill(0)
+      .map((o) => new Animated.Value(o))
+  ).current;
 
   //memakai useEffect otomatis animatednya ditriger dari peruabahab UI
   useEffect(() => {
     //pakai aninated timing dari opacity 0 ke 1 dilambatkan berkesan kedap-kedip
-    const doShowAnimation = [
-      Animated.timing(dot1Opacity, {
-        toValue: 1,
-        duration: 700, //1 dot time utk dari putih ke grey butuh waktu 700ms
-        easing: Easing.ease,
-        useNativeDriver: false, //ini mode lambat
-      }),
-      //2
-
-      Animated.timing(dot2Opacity, {
+    //yg ini kita ringkas
+    const doShowAnimations = dotOpacities.map((opacity) =>
+      Animated.timing(opacity, {
         toValue: 1,
         duration: 700,
         easing: Easing.ease,
-        useNativeDriver: false, //ini mode lambat
-      }),
+        useNativeDriver: false,
+      })
+    );
 
-      //3
-      Animated.timing(dot3Opacity, {
-        toValue: 1,
-        duration: 700,
-        easing: Easing.ease,
-        useNativeDriver: false, //ini mode lambat
-      }),
-    ];
+    // const doShowAnimation = [
+    //   Animated.timing(dot1Opacity, {
+    //     toValue: 1,
+    //     duration: 700, //1 dot time utk dari putih ke grey butuh waktu 700ms
+    //     easing: Easing.ease,
+    //     useNativeDriver: false, //ini mode lambat
+    //   }),
+    //   //2
+
+    //   Animated.timing(dot2Opacity, {
+    //     toValue: 1,
+    //     duration: 700,
+    //     easing: Easing.ease,
+    //     useNativeDriver: false, //ini mode lambat
+    //   }),
+
+    //   //3
+    //   Animated.timing(dot3Opacity, {
+    //     toValue: 1,
+    //     duration: 700,
+    //     easing: Easing.ease,
+    //     useNativeDriver: false, //ini mode lambat
+    //   }),
+    // ];
 
     //matikan 3 3nya /hide
-    const dotHideAniations = [
-      Animated.timing(dot1Opacity, {
+    const dotHideAnimations = dotOpacities.map((opacity) =>
+      Animated.timing(opacity, {
         toValue: 0,
         duration: 500,
         easing: Easing.ease,
-        useNativeDriver: false, //ini mode lambat
-      }),
-      Animated.timing(dot2Opacity, {
-        toValue: 0,
-        duration: 500,
-        easing: Easing.ease,
-        useNativeDriver: false, //ini mode lambat
-      }),
-      Animated.timing(dot3Opacity, {
-        toValue: 0,
-        duration: 500,
-        easing: Easing.ease,
-        useNativeDriver: false, //ini mode lambat
-      }),
-    ];
+        useNativeDriver: false,
+      })
+    );
+
+    // const dotHideAniations = [
+    //   Animated.timing(dot1Opacity, {
+    //     toValue: 0,
+    //     duration: 500,
+    //     easing: Easing.ease,
+    //     useNativeDriver: false, //ini mode lambat
+    //   }),
+    //   Animated.timing(dot2Opacity, {
+    //     toValue: 0,
+    //     duration: 500,
+    //     easing: Easing.ease,
+    //     useNativeDriver: false, //ini mode lambat
+    //   }),
+    //   Animated.timing(dot3Opacity, {
+    //     toValue: 0,
+    //     duration: 500,
+    //     easing: Easing.ease,
+    //     useNativeDriver: false, //ini mode lambat
+    //   }),
+    // ];
 
     //sequencenya
     const sequence = Animated.sequence([
-      Animated.stagger(200, doShowAnimation),
+      Animated.stagger(200, doShowAnimations),
       Animated.delay(300), // ini delay 1 dot  ke dot yg lain
-      Animated.parallel(dotHideAniations),
+      Animated.parallel(dotHideAnimations),
     ]);
 
     //loop sequencenya
@@ -87,17 +118,20 @@ const LoadingDots = () => {
   //stlah 3 smua dot di hide ilang
 
   return (
-    //
-    // <FlowRow>
-    //   <View style={styles.dot} />
-    //   <View style={styles.dot} />
-    //   <View style={styles.dot} />
-    // </FlowRow>
     <FlowRow>
-      <Animated.View style={{ ...styles.dot, opacity: dot1Opacity }} />
-      <Animated.View style={{ ...styles.dot, opacity: dot2Opacity }} />
-      <Animated.View style={{ ...styles.dot, opacity: dot3Opacity }} />
+      {dotOpacities.map((opacity, index) => {
+        <Animated.View
+          key={`dot-${index}`}
+          style={{ ...styles.dot, opacity }}
+        />;
+      })}
     </FlowRow>
+
+    // <FlowRow>
+    //   <Animated.View style={{ ...styles.dot, opacity: dot1Opacity }} />
+    //   <Animated.View style={{ ...styles.dot, opacity: dot2Opacity }} />
+    //   <Animated.View style={{ ...styles.dot, opacity: dot3Opacity }} />
+    // </FlowRow>
   );
 };
 const styles = StyleSheet.create({
